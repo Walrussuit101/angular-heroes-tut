1c5b850f389782b6a92750b7f3a22f8110a8cb29
Walrussuit101 <tjefferson@ycp.edu>
2021-06-02 22:16:57 -0400

    can now delete heroes from the list

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -52,6 +52,14 @@ export class HeroService {
     )
   }
 
+  deleteHero(id: number): Observable<Hero>{
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    )
+  }
+
   //log a HeroService message with the MessageService
   private log(message: string){
     this.messageService.add(`HeroService: ${message}`);
